32dc2336cc884c3ae69902fd604b6f0e111eed54
evgen1000end <evgen1000end@gmail.com>
2015-11-07 10:11:28 +0300

    Add first LinkedList Python implementation

---
+++ LinkedList/python/Basic/Main.py
@@ -0,0 +1,16 @@
+from Basic.LinkedList import LinkedList
+
+linkedList = LinkedList()
+linkedList.insert(10)
+linkedList.insert(12)
+linkedList.insert(13)
+
+linkedList.print()
+
+linkedList.remove(10)
+
+linkedList.print()
+
+
+
+
